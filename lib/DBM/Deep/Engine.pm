8dcee5320e23322d338c6073403171401f5048d6
sprout <sprout@Pint.local>
2010-04-22 20:08:26 -0700

    More spelling fixes

--- lib/DBM/Deep/Engine.pm
+++ lib/DBM/Deep/Engine.pm
@@ -109,7 +109,7 @@ Other DBMSes use transaction logs (a separate file, generally) to achieve
 Durability.  As DBM::Deep is a single-file, we would have to do something
 similar to what SQLite and BDB do in terms of committing using synchronized
 writes. To do this, we would have to use a much higher RAM footprint and some
-serious programming that make my head hurts just to think about it.
+serious programming that makes my head hurt just to think about it.
 
 =cut
 
